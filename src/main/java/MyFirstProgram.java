c9364a0459e2f29bfdbc5427f3ab86a8f7e334ca
Kola Ra <santaramus@gmail.com>
2019-11-12 15:56:56 +0300

    MyFirstProgram.java

--- src/main/java/MyFirstProgram.java
+++ src/main/java/MyFirstProgram.java
@@ -1,6 +1,21 @@
-public class MyFirstProgram{
+public class MyFirstProgram {
 
-	public static void main(String[] args){
-		System.out.println("Hello, world");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+
+        double l = 5;
+        System.out.println("Area square with side " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Area square with sides " + a + " and " + b + " = " + area(a, b));
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
